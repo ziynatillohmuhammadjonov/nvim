720b08e03c0d333c70e04b47de1843486d028a22
Ziynatilloh <ziynatillomuhammadjonov@gmail.com>
2026-01-04 09:37:06 +0500

    Added hot key for figitive.lua

--- lua/plugins/figitive.lua
+++ lua/plugins/figitive.lua
@@ -7,5 +7,17 @@ return {
 		dependencies = {
 			"tpope/vim-fugitive",
 		},
+		keys = {
+			-- Git Status (Oynani ochish)
+			{ "<leader>gs", vim.cmd.Git, desc = "Git Status" },
+			-- Git Push (Kodlarni serverga yuborish)
+			{ "<leader>gp", ":Git push<CR>", desc = "Git Push" },
+			-- Git Pull (Serverdan yangilanishlarni olish)
+			{ "<leader>gl", ":Git pull<CR>", desc = "Git Pull" },
+			-- Git Commit (Xabar bilan saqlash)
+			{ "<leader>gc", ":Git commit<CR>", desc = "Git Commit" },
+			-- Git Log (Flog grafikali tarix)
+			{ "<leader>gh", ":Flog<CR>", desc = "Git Log (Graph)" },
+		},
 	},
 }

2cf862fa20e87e87c15e0b488b4201429c0f17f0
Ziynatilloh <ziynatillomuhammadjonov@gmail.com>
2026-01-04 09:31:53 +0500

    Added some changes

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+		},
+	},
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,44 +8,86 @@ return {
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
-					local opts = { buffer = ev.buf }
+					local function map(mode, lhs, rhs, desc)
+						vim.keymap.set(mode, lhs, rhs, { buffer = ev.buf, desc = "LSP: " .. desc })
+					end
 
-					-- ENGI MUHIMI: Definition - Funksiya yozilgan joyga o'tish
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					-- --- NAVIGATSIYA ---
 
-					-- Declaration - E'lon qilingan joyga o'tish
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-					-- Hover - Funksiya haqida ma'lumot chiqarish
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-					-- Implementation - Interfeys amalga oshirilgan joy
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-
-					-- References - Bu funksiya qayerlarda ishlatilganini ko'rish
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-
-					-- Rename - O'zgaruvchi nomini hamma joyda o'zgartirish
-					vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, opts)
-
-					-- Code Action - Avtomatik tuzatishlar (Import qo'shish va h.k.)
-					vim.keymap.set({ "n", "v" }, "<Leader>la", function()
-						local snacks = require("snacks")
-						if snacks.picker and snacks.picker.lsp_code_actions then
-							snacks.picker.lsp_code_actions()
-						elseif snacks.picker and snacks.picker.lsp_actions then
-							snacks.picker.lsp_actions() -- Ba'zi versiyalarda shunday nomlanadi
+					-- Funksiya yoki o'zgaruvchi yozilgan joyga sakrash (Definition)
+					map("n", "gd", function()
+						if _G.Snacks then
+							Snacks.picker.lsp_definitions()
 						else
-							-- Agar picker topilmasa, standart LSP menyusiga qaytish (xato bermasligi uchun)
+							vim.lsp.buf.definition()
+						end
+					end, "Ta'rifga o'tish (Definition)")
+
+					-- Bu funksiya qayerlarda ishlatilganini ro'yxatda ko'rish (References)
+					map("n", "gr", function()
+						if _G.Snacks then
+							Snacks.picker.lsp_references()
+						else
+							vim.lsp.buf.references()
+						end
+					end, "Qayerda ishlatilganini ko'rish (References)")
+
+					-- Interfeys amalga oshirilgan (Implementation) joyga o'tish
+					map("n", "gi", function()
+						if _G.Snacks then
+							Snacks.picker.lsp_implementations()
+						else
+							vim.lsp.buf.implementation()
+						end
+					end, "Amalga oshirilgan joyga o'tish (Implementation)")
+
+					-- Tur ta'rifiga o'tish (Type Definition)
+					map("n", "gt", function()
+						if _G.Snacks then
+							Snacks.picker.lsp_type_definitions()
+						else
+							vim.lsp.buf.type_definition()
+						end
+					end, "Tur ta'rifiga o'tish (Type Definition)")
+
+					-- E'lon qilingan (Declaration) joyga o'tish
+					map("n", "gD", vim.lsp.buf.declaration, "E'lon qilingan joyga o'tish (Declaration)")
+
+					-- --- MA'LUMOT VA TAHRIR ---
+
+					-- Kursor ostidagi element haqida ma'lumot (Documentation)
+					map("n", "K", vim.lsp.buf.hover, "Hujjatni ko'rish (Hover)")
+
+					-- O'zgaruvchi nomini butun loyiha bo'ylab o'zgartirish
+					map("n", "<Leader>lr", vim.lsp.buf.rename, "Nomni o'zgartirish (Rename)")
+
+					-- Avtomatik tuzatishlar va tezkor amallar (Importlar, fixlar)
+					map({ "n", "v" }, "<Leader>la", function()
+						local snacks = require("snacks")
+						if snacks.picker and (snacks.picker.lsp_code_actions or snacks.picker.lsp_actions) then
+							local action = snacks.picker.lsp_code_actions or snacks.picker.lsp_actions
+							action()
+						else
 							vim.lsp.buf.code_action()
 						end
-					end, opts)
-					--vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts)
+					end, "Tezkor amallar (Code Action)")
 
-					-- Format - Kodni chiroyli qilish
-					vim.keymap.set("n", "<Leader>lf", function()
+					-- Imlo va sintaksis xatolar ro'yxatini ko'rish (Diagnostics)
+					map("n", "<Leader>ld", function()
+						if _G.Snacks then
+							Snacks.picker.diagnostics_buffer()
+						else
+							vim.diagnostic.setloclist()
+						end
+					end, "Fayl xatolarini ko'rish (Diagnostics)")
+
+					-- Kodni formatlash (Conform ishlatilmasa buni ishlatsa bo'ladi)
+					map("n", "<Leader>lf", function()
 						vim.lsp.buf.format({ async = true })
-					end, opts)
+					end, "Kodni formatlash")
+
+					-- Kursor ostidagi xato haqida to'liq ma'lumot (Floating window)
+					map("n", "gl", vim.diagnostic.open_float, "Xato tafsilotini ko'rish")
 				end,
 			})
 		end,

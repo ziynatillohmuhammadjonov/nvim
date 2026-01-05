44ea517cb680e6dadf12017f30a5187ec2e5f4f3
Ziynatilloh <zmozugrand@gmail.com>
2026-01-05 17:24:48 +0500

    Added some plugins

---
+++ lua/plugins/ts-context.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = false,
+		opts = {
+			enable_autocmd = false, -- Avtomatik buyruqni o'chirish (Comment.nvim o'zi boshqaradi)
+		},
+	},
+}

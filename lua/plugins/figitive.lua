f7127e8c24d5743b8f72eeb9bc2e6fbc4ed8c958
Ziynatilloh <ziynatillomuhammadjonov@gmail.com>
2026-01-31 16:19:32 +0500

    Added figitive keymap

--- lua/plugins/figitive.lua
+++ lua/plugins/figitive.lua
@@ -10,21 +10,46 @@ return {
 		keys = {
 			-- Git Status
 			{ "<leader>gs", vim.cmd.Git, desc = "Git Status" },
+
 			-- Git Push
 			{ "<leader>gp", ":Git push<CR>", desc = "Git Push" },
+
 			-- Git Pull
 			{ "<leader>gl", ":Git pull<CR>", desc = "Git Pull" },
+
 			-- Git Commit
 			{ "<leader>gc", ":Git commit<CR>", desc = "Git Commit" },
-			-- Git Log
+
+			-- Git Log (Graph)
 			{ "<leader>gh", ":Flog<CR>", desc = "Git Log (Graph)" },
 
-			-- Hamma branchlarni ro'yxatini ko'rish
+			-- Branch list
 			{ "<leader>gb", ":Git branch<CR>", desc = "Git Branch (List)" },
-			-- Branchlar orasida o'tish (Checkout)
+
+			-- Branch switch
 			{ "<leader>go", ":Git checkout ", desc = "Git Checkout (Switch branch)" },
-			-- Yangi branch ochish
+
+			-- New branch
 			{ "<leader>gn", ":Git checkout -b ", desc = "Git New Branch" },
+
+			----------------------------------------------------------------
+			-- 🆕 PRO FEATURES 🔥
+			----------------------------------------------------------------
+
+			-- So‘nggi commit diff (full preview)
+			{ "<leader>gd", ":Git show<CR>", desc = "Git Show Last Commit Diff" },
+
+			-- So‘nggi commitda o‘zgargan fayllar
+			{ "<leader>gf", ":Git show --name-only<CR>", desc = "Git Last Commit Files" },
+
+			-- Commit history (oneline)
+			{ "<leader>gL", ":Git log --oneline<CR>", desc = "Git Log (Oneline)" },
+
+			-- Hozirgi fayl history
+			{ "<leader>gH", ":0Gclog<CR>", desc = "Git File History" },
+
+			-- HEAD diff (working tree vs last commit)
+			{ "<leader>gD", ":Gvdiffsplit<CR>", desc = "Git Diff (Working tree vs HEAD)" },
 		},
 	},
 }
